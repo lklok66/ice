d1dc321282923f21001a17714f85e10f89f651ca
Bernard Normier <bernard@zeroc.com>
2006-01-06 21:39:28 +0000

    Fix for bug #728 (fixed!)

--- cpp/src/Freeze/Util.cpp
+++ cpp/src/Freeze/Util.cpp
@@ -60,7 +60,7 @@ Freeze::handleDbException(const DbException& dx,
 #if (DB_VERSION_MAJOR == 4) && (DB_VERSION_MINOR == 2)
 	(dx.get_errno() == ENOMEM);
 #else
-        (dx.get_errno() == DB_BUFFER_SMALL || dx.get_errno() == DB_BUFFER_SMALL);
+        (dx.get_errno() == DB_BUFFER_SMALL || dx.get_errno() == ENOMEM);
 #endif	
 
     bool resized = false;	
