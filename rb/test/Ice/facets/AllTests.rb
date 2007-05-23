8f3cf0c6d6a8fbdfec24d974effae87b981190df
Mark Spruiell <mes@zeroc.com>
2007-05-23 23:13:38 +0000

    adding more downcast tests

--- rb/test/Ice/facets/AllTests.rb
+++ rb/test/Ice/facets/AllTests.rb
@@ -22,15 +22,51 @@ def allTests(communicator)
     test(db)
     puts "ok"
 
+    print "testing unchecked cast... "
+    STDOUT.flush
+    obj = Ice::ObjectPrx::uncheckedCast(db)
+    test(obj.ice_getFacet().empty?)
+    obj = Ice::ObjectPrx::uncheckedCast(db, "facetABCD")
+    test(obj.ice_getFacet() == "facetABCD")
+    obj2 = Ice::ObjectPrx::uncheckedCast(obj)
+    test(obj2.ice_getFacet() == "facetABCD")
+    obj3 = Ice::ObjectPrx::uncheckedCast(obj, "")
+    test(obj3.ice_getFacet().empty?)
+    d = Test::DPrx::uncheckedCast(db)
+    test(d.ice_getFacet().empty?)
+    df = Test::DPrx::uncheckedCast(db, "facetABCD")
+    test(df.ice_getFacet() == "facetABCD")
+    df2 = Test::DPrx::uncheckedCast(df)
+    test(df2.ice_getFacet() == "facetABCD")
+    df3 = Test::DPrx::uncheckedCast(df, "")
+    test(df3.ice_getFacet().empty?)
+    puts "ok"
+
     print "testing checked cast... "
     STDOUT.flush
+    obj = Ice::ObjectPrx::checkedCast(db)
+    test(obj.ice_getFacet().empty?)
+    obj = Ice::ObjectPrx::checkedCast(db, "facetABCD")
+    test(obj.ice_getFacet() == "facetABCD")
+    obj2 = Ice::ObjectPrx::checkedCast(obj)
+    test(obj2.ice_getFacet() == "facetABCD")
+    obj3 = Ice::ObjectPrx::checkedCast(obj, "")
+    test(obj3.ice_getFacet().empty?)
     d = Test::DPrx::checkedCast(db)
-    test(d)
-    test(d == db)
+    test(d.ice_getFacet().empty?)
+    df = Test::DPrx::checkedCast(db, "facetABCD")
+    test(df.ice_getFacet() == "facetABCD")
+    df2 = Test::DPrx::checkedCast(df)
+    test(df2.ice_getFacet() == "facetABCD")
+    df3 = Test::DPrx::checkedCast(df, "")
+    test(df3.ice_getFacet().empty?)
     puts "ok"
 
     print "testing non-facets A, B, C, and D... "
     STDOUT.flush
+    d = Test::DPrx::checkedCast(db)
+    test(d)
+    test(d == db)
     test(d.callA() == "A")
     test(d.callB() == "B")
     test(d.callC() == "C")
@@ -41,6 +77,7 @@ def allTests(communicator)
     STDOUT.flush
     df = Test::DPrx::checkedCast(d, "facetABCD")
     test(df)
+    test(df.ice_getFacet() == "facetABCD")
     test(df.callA() == "A")
     test(df.callB() == "B")
     test(df.callC() == "C")
