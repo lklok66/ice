222267507cceb0783572e598d1696aeeaf8e3672
Dwayne Boone <dwayne@zeroc.com>
2007-08-06 09:57:49 -0230

    Fixed Glacier2 demo expect scripts

--- cpp/demo/Glacier2/chat/expect.py
+++ cpp/demo/Glacier2/chat/expect.py
@@ -34,7 +34,7 @@ client1.expect('password:')
 client1.sendline("foo")
 print "ok"
 
-print "starting client 1...",
+print "starting client 2...",
 sys.stdout.flush()
 client2 = DemoUtil.spawn('./client')
 client2.expect('user id:')
