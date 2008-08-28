440c1a4f8d2e8f28d07b169695e4f3a98a387779
Benoit Foucher <benoit@zeroc.com>
2008-08-28 18:32:56 +0200

    - Fixed build system to not build static libraries for Ice & IceUtil - Locator.m, Router.m and Identity.m are now generated from the Slice - Many minor marshalling bug fixes

---
+++ objc/src/Ice/IdentityI.mm
@@ -0,0 +1,46 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2008 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+#import <Ice/IdentityI.h>
+#import <Ice/Util.h>
+
+@implementation ICEIdentity (Internal)
+
+-(ICEIdentity*) initWithIdentity:(const Ice::Identity&)arg
+{
+    if(![super init])
+    {
+        return nil;
+    }
+    category = [[NSString alloc] initWithUTF8String:arg.category.c_str()];
+    name = [[NSString alloc] initWithUTF8String:arg.name.c_str()];
+    return self;
+}
+
+-(Ice::Identity) identity__
+{
+    Ice::Identity ident;
+    ident.category = fromNSString(category);
+    ident.name = fromNSString(name);
+    return ident;
+}
+
+-(void) dealloc
+{
+    [name release];
+    [category release];
+    [super dealloc];
+}
+
++(ICEIdentity*) identityWithIdentity:(const Ice::Identity&)arg
+{
+    return [[[ICEIdentity alloc] initWithIdentity:arg] autorelease];
+}
+
+@end
