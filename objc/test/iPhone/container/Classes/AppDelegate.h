bcdf96e483027a6a7319650d9051cee4c0957db7
Matthew Newhook <matthew@zeroc.com>
2008-09-30 11:34:44 -0230

    Added tests back.

---
+++ objc/test/iPhone/container/Classes/AppDelegate.h
@@ -0,0 +1,34 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2008 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+#import <UIKit/UIKit.h>
+
+@class TestViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate>
+{
+@private
+    UIWindow *window;
+    IBOutlet UINavigationController *navigationController;
+    NSArray* tests;
+    int currentTest;
+    BOOL autoLaunch;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController* navigationController;
+@property (nonatomic, readonly) NSArray* tests;
+@property (nonatomic) int currentTest;
+@property (nonatomic) BOOL autoLaunch;
+
+-(void)testCompleted:(BOOL)success;
+-(void)setAutoLaunch;
+
+@end
+
---
+++ objc/test/iPhone/container/Classes/TestViewController.h
@@ -0,0 +1,33 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2008 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
+#import <UIKit/UIKit.h>
+
+@class Test;
+
+@interface TestViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
+{
+@private
+    IBOutlet UITableView* output;
+    IBOutlet UIActivityIndicatorView* activity;
+    IBOutlet UIButton* nextButton;
+    
+    NSMutableString* currentMessage;
+    NSMutableArray* messages;
+    NSOperationQueue* queue;
+    Test* test;
+    int completed;
+}
+
+@property (retain) Test* test;
+
+-(IBAction)next:(id)sender;
+
+@end
+
