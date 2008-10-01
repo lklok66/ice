306c40fbb9335037a05e06610cca0c27d99d28ed
Michi Henning <michi@zeroc.com>
2008-10-01 11:02:46 +1000

    Added missing copyright notice.

--- objc/demo/book/simple_filesystem/Server.m
+++ objc/demo/book/simple_filesystem/Server.m
@@ -1,3 +1,12 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2008 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
 #import <Ice/Ice.h>
 #import <FileI.h>
 #import <DirectoryI.h>
