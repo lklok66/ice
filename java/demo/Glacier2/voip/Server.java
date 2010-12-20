7cdc1c03860ba31c0284ca9b2d5cfcaba3a72454
Jose <jose@zeroc.com>
2010-12-20 10:09:56 +0100

    Voip minor fixes

--- java/demo/Glacier2/voip/Server.java
+++ java/demo/Glacier2/voip/Server.java
@@ -28,113 +28,113 @@ import Voip._SessionDisp;
 
 public class Server extends Ice.Application
 {
-	private final ScheduledExecutorService timer = new ScheduledThreadPoolExecutor(1);
-	
-	class SessionI extends _SessionDisp
-	{
-		private ControlPrx _ctrl;
-		private long _timestamp = System.currentTimeMillis();
+    private final ScheduledExecutorService timer = new ScheduledThreadPoolExecutor(1);
 
-		@Override
-		public void setControl(ControlPrx ctrl, Current current)
-		{
-			_ctrl = ctrl;
-		}
+    class SessionI extends _SessionDisp
+    {
+        private ControlPrx _ctrl;
+        private long _timestamp = System.currentTimeMillis();
 
-		@Override
-		public void simulateCall(int delay, Current current)
-		{
-			timer.schedule(new Runnable()
-			{
-				@Override
-				public void run()
-				{
-					if(_ctrl != null)
-					{
-						System.out.println("calling incoming call");
-						_ctrl.begin_incomingCall(new Callback_Control_incomingCall()
-						{
-							@Override
-							public void exception(LocalException ex)
-							{
-								System.out.println("incoming call failed");
-								ex.printStackTrace();
-							}
+        @Override
+        public void setControl(ControlPrx ctrl, Current current)
+        {
+            _ctrl = ctrl;
+        }
 
-							@Override
-							public void response()
-							{
-								System.out.println("incoming call succeeded");
-							}
-						});
-					}
-				}
-			}, delay, TimeUnit.MILLISECONDS);
-		}
+        @Override
+        public void simulateCall(int delay, Current current)
+        {
+            timer.schedule(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    if(_ctrl != null)
+                    {
+                        System.out.println("calling incoming call");
+                        _ctrl.begin_incomingCall(new Callback_Control_incomingCall()
+                            {
+                                @Override
+                                public void exception(LocalException ex)
+                                {
+                                    System.out.println("incoming call failed");
+                                    ex.printStackTrace();
+                                }
 
-		@Override
-		public void destroy(Current current)
-		{
-			try
-			{
-				current.adapter.remove(current.id);
-			}
-			catch(NotRegisteredException ex)
-			{
-				// Ignore.
-			}
-		}
+                                @Override
+                                public void response()
+                                {
+                                    System.out.println("incoming call succeeded");
+                                }
+                            });
+                    }
+                }
+            }, delay, TimeUnit.MILLISECONDS);
+        }
 
-		@Override
-		public void refresh(Current current)
-		{
-			_timestamp = System.currentTimeMillis();
-		}
-		
-		long getTimestamp()
-		{
-			return _timestamp;
-		}
-	}
-	
-	class PermissionsVerifierI extends _PermissionsVerifierDisp
-	{
-		@Override
-		public boolean checkPermissions(String userId, String password,
-				StringHolder reason, Current current)
-		{
-			return true;
-		}
-	}
-	
-	class SessionManagerI extends _SessionManagerDisp
-	{
+        @Override
+        public void destroy(Current current)
+        {
+            try
+            {
+                current.adapter.remove(current.id);
+            }
+            catch(NotRegisteredException ex)
+            {
+                // Ignore.
+            }
+        }
 
-		@Override
-		public SessionPrx create(String userId, SessionControlPrx control,
-				Current current) throws CannotCreateSessionException
-		{
-			 // The configured timeout must be greater than 600. This is 601 * 2.
-			final long sessionTimeout = 1202;
-			final SessionI session = new SessionI();
-			final SessionPrx proxy = SessionPrxHelper.uncheckedCast(current.adapter.addWithUUID(session));
-			timer.scheduleWithFixedDelay(new Runnable()
-			{
-			    @Override
-			    public void run()
-			    {
-					// If the session has already been destroyed the ONE will
-					// fall out of run canceling the task.
-					if (System.currentTimeMillis() - session.getTimestamp() > (sessionTimeout * 1000L * 2)) {
-					    proxy.destroy();
-					    throw new ObjectNotExistException();
-					}
-			    }
-			}, sessionTimeout, sessionTimeout, TimeUnit.SECONDS);
-			
-			return proxy;
-		}
-	};
+        @Override
+        public void refresh(Current current)
+        {
+            _timestamp = System.currentTimeMillis();
+        }
+        
+        long getTimestamp()
+        {
+            return _timestamp;
+        }
+    }
+
+    class PermissionsVerifierI extends _PermissionsVerifierDisp
+    {
+        @Override
+        public boolean checkPermissions(String userId, String password, StringHolder reason, Current current)
+        {
+            return true;
+        }
+    }
+
+    class SessionManagerI extends _SessionManagerDisp
+    {
+
+        @Override
+        public SessionPrx create(String userId, SessionControlPrx control,
+                Current current) throws CannotCreateSessionException
+        {
+             // The configured timeout must be greater than 600. This is 601 * 2.
+            final long sessionTimeout = 1202;
+            final SessionI session = new SessionI();
+            final SessionPrx proxy = SessionPrxHelper.uncheckedCast(current.adapter.addWithUUID(session));
+            timer.scheduleWithFixedDelay(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        // If the session has already been destroyed the ONE will
+                        // fall out of run canceling the task.
+                        if (System.currentTimeMillis() - session.getTimestamp() > (sessionTimeout * 1000L * 2))
+                        {
+                            proxy.destroy();
+                            throw new ObjectNotExistException();
+                        }
+                    }
+                }, sessionTimeout, sessionTimeout, TimeUnit.SECONDS);
+            
+            return proxy;
+        }
+    };
 	
     public int
     run(String[] args)
