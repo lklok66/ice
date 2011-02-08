c660c85bc2e6b23d5d28cc98428300ba9e12f721
Mark Spruiell <mes@zeroc.com>
2011-02-07 16:59:58 -0800

    updating README files and makedist script

--- makeandroiddist.py
+++ makeandroiddist.py
@@ -14,32 +14,17 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "distribution", "lib"))
 from DistUtils import *
 
 #
-# This is an explicit list of some Ice Android specific files to remove. The
+# This is an explicit list of files to be removed. The
 # other files are all removed by reversing the below list.
 #
 filesToRemove = [
-    "./java/src/ant",
-    "./java/resources",
-    "./java/CHANGES",
-    "./java/INSTALL",
-    "./java/README",
-    "./java/THIRD_PARTY_LICENSE",
-    "./java/THIRD_PARTY_SOURCES",
-    "./scripts/IceStormUtil.py",
-    "./scripts/IceGridAdmin.py",
-    "./java/allDemos.py"
 ]
 
 # List of files & subdirectories to keep, all others are removed.
 filesToKeep = [
     "./LICENSE",
     "./ICE_LICENSE",
-    "./Makefile.android",
-    "./Makefile.mak.android",
-    "./java",
-    "./android",
-    "./scripts",
-    "./certs"
+    "./android"
 ]
 
 def pathInList(p, l):
@@ -82,7 +67,7 @@ def usage():
     print "Options:"
     print "-h      Show this message."
     print "-v      Be verbose."
-    print "-c DIR  Compare distribution to the one from DIR and"
+    print "-c DIR  Compare distribution to the one from DIR"
 
 #
 # Check arguments
@@ -117,100 +102,67 @@ cwd = os.getcwd()
 os.chdir(os.path.dirname(__file__))
 
 #
-# Get version.
+# Ice for Android version.
 #
-config = open(os.path.join("java", "config", "build.properties"), "r")
-version = re.search("iceandroid.version = ([0-9\.]*)", config.read()).group(1)
+version = "0.2"
+
+#
+# Ice tag.
+#
+iceTag = "R3_4_1"
+
+#
+# Patch file.
+#
+patchFile = "android/patch.txt"
+
+distDir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "disticeandroid-" + tag.replace('/', '-')))
+print "Creating IceAndroid " + version + " distributions in " + distDir
 
 #
 # Remove any existing "disticeandroid-" directory and create a new one
 #
-distDir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "disticeandroid-" + tag.replace('/', '-')))
 if os.path.exists(distDir):
     remove(distDir)
 os.mkdir(distDir)
 
-print "Creating IceAndroid " + version + " source distributions in " + distDir
-
 srcDir = os.path.join(distDir, "IceAndroid-" + version)
 
 #
-# Extract the sources with git archive using the given tag.
+# Extract the Ice for Android sources.
 #
-print "Creating git archive using " + tag + "...",
+print "Extracting Ice for Android sources using tag " + tag + "...",
 sys.stdout.flush()
 os.system("git archive --prefix=IceAndroid-" + version + "/ " + tag + " | ( cd " + distDir + " && tar xfm - )")
 print "ok"
 
-os.chdir(os.path.join(srcDir))
-
-print "Fixing makefiles...",
-
 #
-# Change SUBDIRS and INSTALL_SUBDIRS in top-level Makefile.
+# Extract the Ice sources.
 #
-for makeFileName in [os.path.join("cpp", "Makefile.mak"), os.path.join("cpp", "Makefile")]:
-    makeFile = open(makeFileName, "r")
-    lines = makeFile.readlines()
-    makeFile.close()
-    for i in range(len(lines)):
-        if lines[i].find("SUBDIRS") == 0:
-            lines[i] = "SUBDIRS = src\n"
-        if lines[i].find("INSTALL_SUBDIRS") == 0:
-            lines[i] = "INSTALL_SUBDIRS = $(install_bindir) $(install_libdir)\n"
-    makeFile = open(makeFileName, "w")
-    makeFile.writelines(lines)
-    makeFile.close()
-
-#
-# Disable install targets for libIceUtil, libSlice.
-#
-for makeFileName in [os.path.join("cpp", "src", "IceUtil", "Makefile.mak"), \
-                     os.path.join("cpp", "src", "IceUtil", "Makefile"), \
-                     os.path.join("cpp", "src", "Slice", "Makefile.mak"), \
-                     os.path.join("cpp", "src", "Slice", "Makefile")]:
-    makeFile = open(makeFileName, "r")
-    lines = makeFile.readlines()
-    makeFile.close()
-
-    doComment = 0
-    for i in range(len(lines)):
-        if lines[i].find("install::") == 0:
-            doComment = 1
-        elif len(lines[i].strip()) == 0:
-            doComment = 0
-        elif doComment:
-            lines[i] = "#" + lines[i]
-
-    makeFile = open(makeFileName, "w")
-    makeFile.writelines(lines)
-    makeFile.close()
-
-print "ok"
-
-print "Walking through distribution to fix permissions, versions, etc...",
+print "Extracting Ice sources using tag " + iceTag + "...",
 sys.stdout.flush()
-
-#fixVersion(os.path.join("distribution", "bin", "makeiceandroidbindist.py"), version)
-
-bisonFiles = []
-flexFiles = []
-for root, dirnames, filesnames in os.walk('.'):
-
-    for f in filesnames:
-        filepath = os.path.join(root, f) 
-        if f == ".gitignore" or f == "expect.py":
-            os.remove(filepath)
-        else:
-            # Fix version of README/INSTALL files
-            if fnmatch.fnmatch(f, "README*") or fnmatch.fnmatch(f, "INSTALL*"):
-                fixVersion(filepath, version)
-            fixFilePermission(filepath)
-    
-    for d in dirnames:
-        os.chmod(os.path.join(root, d), S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) # rwxr-xr-x
-
+os.system("git archive --prefix=Ice-" + iceTag + "/ " + iceTag + " | ( cd " + distDir + " && tar xfm - )")
 print "ok"
+
+iceDir = os.path.join(distDir, "Ice-" + iceTag)
+
+print "Patching Ice sources..."
+os.chdir(iceDir)
+os.system("patch -p1 < " + os.path.join(srcDir, patchFile))
+
+print "Building Ice for Android JAR file..."
+os.chdir(os.path.join(iceDir, "cpp", "src", "IceUtil"))
+os.system("make")
+os.chdir(os.path.join(iceDir, "cpp", "src", "Slice"))
+os.system("make")
+os.chdir(os.path.join(iceDir, "cpp", "src", "slice2java"))
+os.system("make")
+os.chdir(os.path.join(iceDir, "cpp", "src", "slice2freezej"))
+os.system("make")
+os.chdir(os.path.join(iceDir, "java"))
+os.system("ant ice-jar")
+
+os.chdir(srcDir)
 
 #
 # Remove or move non-public files out of source distribution.
@@ -225,19 +177,16 @@ for x in filesToRemove:
 print "ok"
 
 #
-# Copy IceAndroid specific install files.
+# Move install files.
 #
 print "Copying install files...",
-move(os.path.join("android", "CHANGES"), os.path.join("CHANGES"))
-move(os.path.join("android", "README"), os.path.join("README"))
-move(os.path.join("android", "RELEASE_NOTES"), os.path.join("RELEASE_NOTES"))
-move(os.path.join("android", "INSTALL"), os.path.join("INSTALL"))
-
-move(os.path.join("Makefile.mak.android"), os.path.join("Makefile.mak"))
-move(os.path.join("Makefile.android"), os.path.join("Makefile"))
+move(os.path.join("android", "CHANGES"), "CHANGES")
+move(os.path.join("android", "README"), "README")
+move(os.path.join("android", "RELEASE_NOTES"), "RELEASE_NOTES")
+move(os.path.join("android", "INSTALL"), "INSTALL")
 
 #
-# Copy CHANGES and RELEASE_NOTES
+# Copy CHANGES, README and RELEASE_NOTES.
 #
 copy(os.path.join(srcDir, "CHANGES"), os.path.join(distDir, "IceAndroid-" + version + "-CHANGES"))
 copy(os.path.join(srcDir, "RELEASE_NOTES"), os.path.join(distDir, "IceAndroid-" + version + "-RELEASE_NOTES"))
@@ -246,21 +195,13 @@ copy(os.path.join(srcDir, "README"), os.path.join(distDir, "IceAndroid-" + versi
 print "ok"
 
 #
-# Build the .jar files.
+# Move the JAR file.
 #
-copy("java", "java-build")
-os.chdir("java-build")
-if os.system("ant jar") != 0:
-    print sys.argv[0] + ": failed to the build the Ice Android jar files"
-    sys.exit(1)
-os.chdir("..")
-os.mkdir(os.path.join("java", "lib"))
-move(os.path.join("java-build", "lib", "IceAndroid.jar"), os.path.join("java", "lib", "IceAndroid.jar"))
-move(os.path.join("java-build", "lib", "IceTest.jar"), os.path.join("java", "lib", "IceTest.jar"))
-remove("java-build")
+os.mkdir(os.path.join("android", "lib"))
+move(os.path.join(iceDir, "java", "lib", "Ice.jar"), os.path.join("android", "lib", "Ice.jar"))
 
 #
-# Everything should be clean now, we can create the source distributions archives
+# Everything should be clean now, we can create the distribution archives.
 # 
 print "Archiving..."
 sys.stdout.flush()
@@ -283,6 +224,7 @@ writeSrcDistReport("IceAndroid", version, compareToDir, [srcDir])
 print "Cleaning up...",
 sys.stdout.flush()
 remove(srcDir)
+remove(iceDir)
 print "ok"
 
 os.chdir(cwd)
