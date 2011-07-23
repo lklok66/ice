844dd1c466f233f0ff76b8b65386b7efa93635ef
Jose <jose@zeroc.com>
2011-07-24 01:26:38 +0200

    IceTouch installer: added pre install script to uninstall IceTouch-1.1.0 if present

---
+++ distribution/src/mac/IceTouch/scripts/pre-install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+echo "OK" > /opt/preinstall.txt
+
+#
+# Uninstall IceTouch-1.1.0 SDKs
+#
+if [ -f /var/db/receipts/com.zeroc.pkg.IceTouch-sdk-1.1.bom -a -f /var/db/receipts/com.zeroc.pkg.IceTouch-sdk-1.1.plist ]; then
+    if [ -d /opt/IceTouch-1.1 ]; then
+        rm -rf /opt/IceTouch-1.1
+    fi
+    if [ -d /Developer/SDKs/IceTouch-1.1 ]; then
+        rm -rf /Developer/SDKs/IceTouch-1.1
+    fi
+
+    if [ -d /Developer/Examples/IceTouch-1.1 ]; then
+        rm -rf /Developer/Examples/IceTouch-1.1
+    fi
+
+    if [ -d /Developer/Documentation/IceTouch-1.1 ]; then
+        rm -rf /Developer/Documentation/IceTouch-1.1
+    fi
+
+    rm -rf /var/db/receipts/com.zeroc.pkg.IceTouch-sdk-1.1.bom
+    rm -rf /var/db/receipts/com.zeroc.pkg.IceTouch-sdk-1.1.plist
+fi
+
+#
+# Uninstall IceTouch-1.1.0 Xcode plug-in
+#
+if [ -f /var/db/receipts/com.zeroc.pkg.IceTouch-Xcode-Plugin.bom -a -f /var/db/receipts/com.zeroc.pkg.IceTouch-Xcode-Plugin.plist ]; then
+    if [ -d /Developer/Library/Xcode/Plug-ins/slice2objcplugin.pbplugin ]; then
+        rm -rf /Developer/Library/Xcode/Plug-ins/slice2objcplugin.pbplugin
+    fi
+    rm -rf /var/db/receipts/com.zeroc.pkg.IceTouch-Xcode-Plugin.bom
+    rm -rf /var/db/receipts/com.zeroc.pkg.IceTouch-Xcode-Plugin.plist
+fi
