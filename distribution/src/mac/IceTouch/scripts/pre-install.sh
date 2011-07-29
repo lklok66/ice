631daa6d2a111d547ddddca0256da09b0c3dc423
Jose <jose@zeroc.com>
2011-07-29 20:26:04 +0200

    Fix pre-install.sh to also uninstall plug-in from previous source distrbutions

--- distribution/src/mac/IceTouch/scripts/pre-install.sh
+++ distribution/src/mac/IceTouch/scripts/pre-install.sh
@@ -3,33 +3,46 @@
 #
 # Uninstall IceTouch-1.1.0 SDKs
 #
-if [ -f /var/db/receipts/com.zeroc.pkg.IceTouch-sdk-1.1.bom -a -f /var/db/receipts/com.zeroc.pkg.IceTouch-sdk-1.1.plist ]; then
-    if [ -d /opt/IceTouch-1.1 ]; then
-        rm -rf /opt/IceTouch-1.1
-    fi
-    if [ -d /Developer/SDKs/IceTouch-1.1 ]; then
-        rm -rf /Developer/SDKs/IceTouch-1.1
-    fi
-
-    if [ -d /Developer/Examples/IceTouch-1.1 ]; then
-        rm -rf /Developer/Examples/IceTouch-1.1
-    fi
-
-    if [ -d /Developer/Documentation/IceTouch-1.1 ]; then
-        rm -rf /Developer/Documentation/IceTouch-1.1
-    fi
-
+if [ -f /var/db/receipts/com.zeroc.pkg.IceTouch-sdk-1.1.bom ]; then
     rm -rf /var/db/receipts/com.zeroc.pkg.IceTouch-sdk-1.1.bom
+fi
+
+if [ -f /var/db/receipts/com.zeroc.pkg.IceTouch-sdk-1.1.plist ]; then
     rm -rf /var/db/receipts/com.zeroc.pkg.IceTouch-sdk-1.1.plist
+fi
+
+if [ -d /opt/IceTouch-1.1 ]; then
+    rm -rf /opt/IceTouch-1.1
+fi
+
+if [ -d /Developer/SDKs/IceTouch-1.1 ]; then
+    rm -rf /Developer/SDKs/IceTouch-1.1
+fi
+
+if [ -d /Developer/Examples/IceTouch-1.1 ]; then
+    rm -rf /Developer/Examples/IceTouch-1.1
+fi
+
+if [ -d /Developer/Documentation/IceTouch-1.1 ]; then
+    rm -rf /Developer/Documentation/IceTouch-1.1
 fi
 
 #
 # Uninstall IceTouch-1.1.0 Xcode plug-in
 #
-if [ -f /var/db/receipts/com.zeroc.pkg.IceTouch-Xcode-Plugin.bom -a -f /var/db/receipts/com.zeroc.pkg.IceTouch-Xcode-Plugin.plist ]; then
-    if [ -d /Developer/Library/Xcode/Plug-ins/slice2objcplugin.pbplugin ]; then
-        rm -rf /Developer/Library/Xcode/Plug-ins/slice2objcplugin.pbplugin
-    fi
+
+if [ -d /Developer/Library/Xcode/Plug-ins/slice2objcplugin.pbplugin ]; then
+    rm -rf /Developer/Library/Xcode/Plug-ins/slice2objcplugin.pbplugin
+fi
+
+if [ -d "$HOME/Library/Application Support/Developer/Shared/Xcode/Plug-ins/slice2objcplugin.pbplugin" ]; then
+    rm -rf "$HOME/Library/Application Support/Developer/Shared/Xcode/Plug-ins/slice2objcplugin.pbplugin"
+fi
+
+if [ -f /var/db/receipts/com.zeroc.pkg.IceTouch-Xcode-Plugin.bom ]; then
     rm -rf /var/db/receipts/com.zeroc.pkg.IceTouch-Xcode-Plugin.bom
+fi
+
+if [ -f /var/db/receipts/com.zeroc.pkg.IceTouch-Xcode-Plugin.plist ]; then
     rm -rf /var/db/receipts/com.zeroc.pkg.IceTouch-Xcode-Plugin.plist
 fi
