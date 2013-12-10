54f4630625464fb71b2775c6350b64edb93644a2
Jose <jose@zeroc.com>
2013-12-10 10:35:35 +0100

    Add Object ice_staticId implementation.

--- js/src/Ice/Object.js
+++ js/src/Ice/Object.js
@@ -12,40 +12,47 @@
 //
 // Using IceObject in this file to avoid collisions with the native Object.
 //
-IceObject = function()
+var IceObject = function()
 {
-}
+};
 
 IceObject.prototype.__notImplemented = function()
 {
     throw new Error("not implemented");
-}
+};
 
 IceObject.prototype.ice_isA = function(s, current)
 {
     return this.ice_ids().indexOf(s) >= 0;
-}
+};
 
 IceObject.prototype.ice_ping = function(current)
 {
-}
+};
+
+IceObject.__ids = ["::Ice::Object"];
 
 IceObject.prototype.ice_ids = function(current)
 {
-    return ["::Ice::Object"];
-}
+    return IceObject.__ids;
+};
+
+IceObject.ice_staticId = function()
+{
+    return IceObject.__ids[0];
+};
 
 IceObject.prototype.ice_id = function(current)
 {
-    return this.ice_ids()[0];
-}
+    return IceObject.__ids[0];
+};
 
 IceObject.prototype.ice_preMarshal = function()
 {
-}
+};
 
 IceObject.prototype.ice_postMarshal = function()
 {
-}
+};
 
 module.exports = IceObject;
