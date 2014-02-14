3c8e63989c039584a466a499d5f55a86c21ca602
Dwayne Boone <dwayne@zeroc.com>
2014-02-14 12:27:54 -0330

    Added batch files to build js demos on Windows

---
+++ js/demo/Ice/latency/build.bat
@@ -0,0 +1,16 @@
+@echo off
+REM **********************************************************************
+REM
+REM Copyright (c) 2003-2014 ZeroC, Inc. All rights reserved.
+REM
+REM This copy of Ice is licensed to you under the terms described in the
+REM ICE_LICENSE file included in this distribution.
+REM
+REM **********************************************************************
+
+REM .bat wrapper for buidling javascript demos. Assumes ICEJS_HOME is set.
+
+@echo Building %~dp0
+
+@echo "%ICEJS_HOME%\bin\slice2js" -I"%ICEJS_HOME%\slice" Latency.ice
+"%ICEJS_HOME%\bin\slice2js" -I"%ICEJS_HOME%\slice" Latency.ice
