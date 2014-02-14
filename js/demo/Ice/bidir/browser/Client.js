efc1acb2c22786fd30644f215cefd6ab10fc58c4
Jose <jose@zeroc.com>
2014-02-14 13:25:16 +0100

    Demo minor fixes

--- js/demo/Ice/bidir/browser/Client.js
+++ js/demo/Ice/bidir/browser/Client.js
@@ -1,3 +1,12 @@
+// **********************************************************************
+//
+// Copyright (c) 2003-2013 ZeroC, Inc. All rights reserved.
+//
+// This copy of Ice is licensed to you under the terms described in the
+// ICE_LICENSE file included in this distribution.
+//
+// **********************************************************************
+
 (function(){
 
 var Promise = Ice.Promise;
@@ -83,7 +92,7 @@ var stop = function()
 $("#start").click(
     function()
     {
-        if(!isConnected())
+        if(isDisconnected())
         {
             setState(State.Connecting);
             Promise.try(
@@ -144,6 +153,11 @@ var State = {
 var isConnected = function()
 {
     return state == State.Connected;
+};
+
+var isDisconnected = function()
+{
+    return state == State.Disconnected;
 };
 
 var writeLine = function(msg)
